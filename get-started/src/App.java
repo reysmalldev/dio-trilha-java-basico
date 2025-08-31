eced2950fcb1fca6ba23f2efefaf408e16738750
Inácio <guilherme.projetoismart@gmail.com>
2025-08-31 11:06:19 -0300

    Basic Sintax course study

---
+++ get-started/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao cadastro de usuários, selecione a operação desejada");
+    }
+}
---
+++ get-started/src/br/com/inacio/dao/UserDAO.java
@@ -0,0 +1,43 @@
+package br.com.inacio.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.inacio.exception.UserNotFoundException;
+import br.com.inacio.model.UserModel;
+
+public class UserDAO {
+    private long nextId = 1L;
+
+    private final List<UserModel> models = new ArrayList<>();
+
+    public UserModel save(final UserModel model) {
+        model.setId(nextId++);
+        models.add(model);
+        return model;
+    }
+
+    public UserModel update(final UserModel model) {
+        var toUpdate = findById(model.getId());
+        models.remove(toUpdate);
+        models.add(model);
+        return model;
+    }
+
+    public void delete(final long id) {
+        var toDelete = findById(id);
+        models.remove(toDelete);
+    }
+
+    public UserModel findById(final long id) {
+        var message = String.format("Não existe usuário com o id %s cadastrado", id);
+        return models.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException(message));
+    }
+
+    public List<UserModel> findAll() {
+        return models;
+    }
+}
